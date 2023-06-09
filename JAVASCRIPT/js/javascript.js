64da0211dceb0f92f37a674be8d5b1e00d4493b8
Nicolas <momboulinicolas@gmail.com>
2023-06-09 12:20:20 +0200

    en cours

---
+++ JAVASCRIPT/js/javascript.js
@@ -0,0 +1,13 @@
+/*exo.js5
+
+
+var reponse1 = prompt  ("saisissez votre nom");
+var reponse2 = prompt ("saissisez votre prénom");
+alert (" ton nom est  " + reponse1 + "\nton prénom est " + reponse2  ); */
+
+var calcule =  parseFloat(prompt("écrire le premier  nombre "));
+var calcule2  = parseFloat(prompt("écrire le deuxième nombre"));
+var calcule3 = calcule * calcule2
+
+alert(" la réponse est " + calcule3  );
+
