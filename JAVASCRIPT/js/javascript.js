ad80e85983eef400ded2575f41f556275e9ec62d
Nicolas <momboulinicolas@gmail.com>
2023-06-14 08:51:31 +0200

    en cours de travaux

--- JAVASCRIPT/js/javascript.js
+++ JAVASCRIPT/js/javascript.js
@@ -1,13 +1,46 @@
-/*exo.js5
+//exo.js5 Affichage du texte
 
-
+/*
+ex1
 var reponse1 = prompt  ("saisissez votre nom");
 var reponse2 = prompt ("saissisez votre prénom");
 alert (" ton nom est  " + reponse1 + "\nton prénom est " + reponse2  ); */
 
-var calcule =  parseFloat(prompt("écrire le premier  nombre "));
-var calcule2  = parseFloat(prompt("écrire le deuxième nombre"));
-var calcule3 = calcule * calcule2
+/* ex1.1
+  var nb1 = parseFloat (prompt ("Veuillez entrez le premier nombre"));
+  var nb2 =parseFloat (prompt (" veuillez entrez le deuxième nombre"));
 
-alert(" la réponse est " + calcule3  );
+*/
+
+ /* ex1.2
+ var tCelsius = prompt("Entrez lantempérature en degrés Celsius : ");
+var tFahrenheit = (tCelsius * 9/5) + 32; */
+
+
+// exo.js6 Les opérateur
+
+/*ex1
+var a = 100
+alert(" Ceci est une chaîne de caractères :  " + a );*/
+
+/* ex1.1
+var b = 100
+var db = b-- ;
+alert(db);
+alert (b) ; */
+
+/* ex1.2
+
+var a = 100;
+var c = 1.00;
+ c += parseFloat (a);
+ alert(c) ; */
+
+
+
+
+
+
+
+
 
